2904ec08c9840f18d74a0e8117dcfe6b1cf4d563
Nixixouwu <ni.aliaga@duocuc.cl>
2024-09-13 12:50:59 -0300

    Login Base

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonIcon, IonButton, IonItem } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { mail, lockClosed } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonItem, IonButton, IonIcon, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class LoginPage implements OnInit {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    // Handle login logic here
+    console.log('Login submitted', this.email, this.password);
+  }
+
+  constructor() { 
+    addIcons({ mail, lockClosed });
+  }
+
+  ngOnInit() {
+  }
+
+}
