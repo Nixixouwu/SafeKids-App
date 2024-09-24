ca9bbaacdf8342f9d967138bccdd17e896a789c5
Nixixouwu <ni.aliaga@duocuc.cl>
2024-09-24 16:44:34 -0300

    Detalles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyBVAI9clBW1HlsmHEu0L4RMGvDhLMEgx_c",
-    authDomain: "safekids-app.firebaseapp.com",
-    projectId: "safekids-app",
-    storageBucket: "safekids-app.appspot.com",
-    messagingSenderId: "597815314630",
-    appId: "1:597815314630:web:01aa4c08edd5af79a4afc7",
-    measurementId: "G-PGMKY6NWC0"
-    }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-  apiKey: "AIzaSyBVAI9clBW1HlsmHEu0L4RMGvDhLMEgx_c",
-  authDomain: "safekids-app.firebaseapp.com",
-  projectId: "safekids-app",
-  storageBucket: "safekids-app.appspot.com",
-  messagingSenderId: "597815314630",
-  appId: "1:597815314630:web:01aa4c08edd5af79a4afc7",
-  measurementId: "G-PGMKY6NWC0"
-  }
+  production: false
 };
 
 /*
