b3738e32b3d58092540139c785aa6f85dcab50cf
Sekod <101645781+Sekod@users.noreply.github.com>
2024-11-05 19:38:51 -0300

    whatsapp y pdf

--- src/app/info-conductor/info-conductor.page.ts
+++ src/app/info-conductor/info-conductor.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Firestore, doc, getDoc } from '@angular/fire/firestore';
+import { Firestore, doc, getDoc, collection, query, where, getDocs } from '@angular/fire/firestore';
+import { jsPDF } from 'jspdf';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -12,6 +13,7 @@ import { IonicModule } from '@ionic/angular';
 })
 export class InfoConductorPage implements OnInit {
   driverInfo: any = {};
+  busInfo: any = {};
 
   constructor(private route: ActivatedRoute, private firestore: Firestore) {}
 
@@ -25,9 +27,22 @@ export class InfoConductorPage implements OnInit {
     const driverDocSnapshot = await getDoc(driverDocRef);
     
     if (driverDocSnapshot.exists()) {
-      this.driverInfo = { id: driverDocSnapshot.id, ...driverDocSnapshot.data() };
+        this.driverInfo = { id: driverDocSnapshot.id, ...driverDocSnapshot.data() };
+        console.log('Información del conductor:', this.driverInfo);
+        
+        const busesRef = collection(this.firestore, 'Bus');
+        const q = query(busesRef, where('FK_BUConductor', '==', this.driverInfo.RUT));
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) {
+            const busData = querySnapshot.docs[0].data();
+            this.busInfo = { id: querySnapshot.docs[0].id, ...busData };
+            console.log('Información del bus:', this.busInfo);
+        } else {
+            console.error('No se encontró información del bus para este conductor');
+        }
     } else {
-      console.error('No se encontró información del conductor para este ID');
+        console.error('No se encontró información del conductor para este ID');
     }
   }
 
@@ -41,7 +56,27 @@ export class InfoConductorPage implements OnInit {
   }
 
   downloadAntecedentes() {
-    // Implementar la lógica para descargar antecedentes
-    console.log('Descargando antecedentes...');
+    const doc = new jsPDF();
+
+    doc.setFontSize(16);
+    doc.text('Información del Conductor', 10, 10);
+    doc.setFontSize(12);
+    doc.text(`Nombre: ${this.driverInfo.Nombre} ${this.driverInfo.Apellido}`, 10, 20);
+    doc.text(`Fecha de Admision: ${this.driverInfo.Fecha_Admision}`, 10, 30);
+    doc.text(`Edad: ${this.driverInfo.Edad} Años`, 10, 40);
+    doc.text(`Dirección: ${this.driverInfo.Direccion}`, 10, 50);
+    doc.text(`Email: ${this.driverInfo.Email}`, 10, 60);
+    doc.text(`Género: ${this.driverInfo.Genero}`, 10, 70);
+    doc.text(`RUT: ${this.driverInfo.RUT}`, 10, 80);
+    doc.text(`Teléfono: ${this.driverInfo.Telefono}`, 10, 90);
+
+    doc.setFontSize(16);
+    doc.text('Información del Bus', 10, 160);
+    doc.setFontSize(12);
+    doc.text(`Patente: ${this.busInfo.ID_Placa}`, 10, 180);
+    doc.text(`Modelo: ${this.busInfo.Modelo}`, 10, 190);
+    
+
+    doc.save('informacion_conductor.pdf');
   }
 }
